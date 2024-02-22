fe87abb387980ee80b19d247fa8d210e6def827f
unknown <drishyacp49@gmail.com>
2024-02-22 13:24:33 +0530

    first t

--- qs2.js
+++ qs2.js
@@ -1,14 +1,4 @@
 
-
-//  const data=[{'name':'bmw','price':2000},
-// {name:'benz',price:1000},
-// {name:'honda',price:500},
-// {name:'ford',price:3000},
-// {name:'hyundai',price:2000},
-// {name:'toyota',price:100},
-// {name:'tata',price:500},
-// {name:'minicooper',price:2000},
-// ];
 covid_data = [
     [1,'Ernakulam',34000,2000,23000,20000,2000],
     [2,'Idukki',14000,3000,25000,30000,1000],
@@ -21,12 +11,10 @@ covid_data = [
 ]
 
 // let highestCase = covid_data.reduce((p1,p2)=>p1[2]>p2[2]?p1:p2)
-
-
 // console.log(highestCase);
 
-let highest_dose=covid_data.reduce((p1,p2)=>p1[5]>p2[5]?p1:p2);
-console.log(highest_dose);
+// let highest_dose=covid_data.reduce((p1,p2)=>p1[5]>p2[5]?p1:p2);
+// console.log(highest_dose);
 
 
 //3. district having lowest death rate - 
@@ -35,18 +23,23 @@ console.log(highest_dose);
 
   
 //4. sort data with +ve case in descending order -
-// let sorted=covid_data.sort((p1,p2)=>p2[2]-p1[2]).forEach(k=>console.log(p[1],p[2]));
+// let sorted=covid_data.sort((p1,p2)=>p2[2]-p1[2]).forEach(k=>console.log(k[1],k[2]));
 // console.log(sorted);
 
 
 //5. is district with +ve cases > 15000 - 
-// let cases_greater=covid_data.filter((item)=>item[2]>15000);
-// console.log(cases_greater);
+// let cases_greater=covid_data.filter((item)=>item[2]>15000).forEach(element => {console.log(element[1],element[2])
+//     });;
+
 
 //6. sort data with 1st dose vaccine - 
+// dose_sorted=covid_data.sort((p1,p2)=>p1[5]-p2[5]).forEach(element => console.log(element[1],element[5]
+// ));
+
 
 //7. Print Thrissur details - 
-// let thissur=covid_data.reduce((item)=>item)
+let thissur=covid_data.filter((item)=>item[1]=='Thrissur').forEach(k=>console.log(k))
+
 
 
 
